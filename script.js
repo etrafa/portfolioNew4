b586a54c899e77101275b7f2a601fbbf7523b827
etrafa <84640450+etrafa@users.noreply.github.com>
2022-01-11 01:32:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+"using strict";
+
+// const googleHomeImageEl = document.querySelector(".google-home-image");
+// const spinAnswerImageEl = document.querySelector(".spin-answer-image");
+// const travelImageEl = document.querySelector(".travel-image");
+// const eCommerceImageEl = document.querySelector(".e-commerce-image");
+// const weatherImageEl = document.querySelector(".weather-image");
+
+// const googleHomeImageSources = [
+//   "./work/google-home-1.png",
+//   "./work/google-home-2.png",
+//   "./work/google-home-3.png",
+// ];
+
+// const spinAnswerImageSources = [
+//   "./work/spin-answer-1.png",
+//   "./work/spin-answer-2.png",
+//   "./work/spin-answer-3.png",
+// ];
+
+// const travelImageSources = [
+//   "./work/travel-1.png",
+//   "./work/travel-2.png",
+//   "./work/travel-3.png",
+// ];
+
+// const eCommerceImageSources = [
+//   "./work/tshirt-1.png",
+//   "./work/tshirt-2.png",
+//   "./work/tshirt-3.png",
+// ];
+
+// const weatherImageSources = [
+//   "./work/weather-1.png",
+//   "./work/weather-2.png",
+//   "./work/weather-3.png",
+// ];
+
+// let googleCounter = 1;
+
+// const changeImageGoogle = () => {
+//   if (googleCounter === 1) {
+//     googleHomeImageEl.src = googleHomeImageSources[0];
+//   }
+//   setTimeout(() => {
+//     googleCounter = googleCounter + 1;
+//     if (googleCounter === 2) {
+//       googleHomeImageEl.src = googleHomeImageSources[1];
+//     }
+//   }, 3000);
+//   setTimeout(() => {
+//     googleCounter = googleCounter + 1;
+//     if (googleCounter === 3) {
+//       googleHomeImageEl.src = googleHomeImageSources[2];
+//     }
+//   }, 6000);
+//   setTimeout(() => {
+//     googleHomeImageEl.src = googleHomeImageSources[0];
+//   }, 7000);
+// };
+
+// changeImageGoogle();
